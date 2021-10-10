107ca951d1d86ef6dbdfc13c9fe977697b8e90ec
Sippakorn Ornwichian <o.sippakorn@gmail.com>
2021-10-10 17:39:01 +0700

    feat: use Container in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import type { AppProps } from 'next/app'
 
+import { Container } from '../components/Container'
 import '../styles/globals.css'
 import { SocketContext, socketInstance } from '../utils/socketUtils'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SocketContext.Provider value={socketInstance}>
-      <Component {...pageProps} />
+      <Container>
+        <Component {...pageProps} />
+      </Container>
     </SocketContext.Provider>
   )
 }

69a8288e8552b31e9b96e2a62ae1cb42cd013673
Sippakorn Ornwichian <o.sippakorn@gmail.com>
2021-10-10 17:39:18 +0700

    feat: add Grid

---
+++ components/Grid/index.tsx
@@ -0,0 +1,34 @@
+import React, { FC } from 'react'
+
+import styled from 'styled-components'
+
+import { mainTheme } from '../../utils/themeConst'
+import { Row } from '../Container'
+import Block from './Block'
+
+const GridContainer = styled.div`
+  border: ${mainTheme.spacing(0.2)} ${mainTheme.secondary} solid;
+  min-height: ${mainTheme.spacing(12)};
+  min-width: ${mainTheme.spacing(12)};
+`
+
+interface GridProps {
+  gridData: number[][]
+}
+
+const Grid: FC<GridProps> = ({ gridData }) => {
+  return (
+    <GridContainer>
+      {gridData.map((gridRow, row) => (
+        <Row key={`GridRow-${row}`}>
+          {gridRow.map((block, column) => {
+            const isBomb: boolean = block >= 2
+            return <Block key={`Block-${row}-${column}`} isBomb={isBomb} />
+          })}
+        </Row>
+      ))}
+    </GridContainer>
+  )
+}
+
+export default Grid

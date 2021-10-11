7705465e08ff7aabb93901421aa72a14ea54503b
Sippakorn Ornwichian <o.sippakorn@gmail.com>
2021-10-11 16:49:38 +0700

    feat: add reusable column flex

--- components/Container.tsx
+++ components/Container.tsx
@@ -3,14 +3,26 @@ import styled from 'styled-components'
 import { mainTheme } from '../utils/themeConst'
 
 export const Container = styled.div`
-  background-color: ${mainTheme.background};
   min-height: 100vh;
   min-width: 100vw;
   height: 100%;
   width: 100vw;
+
+  background-color: ${mainTheme.background};
+  padding: 0;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `
 
 export const Row = styled.div`
   display: flex;
   flex-direction: row;
+`
+
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
 `

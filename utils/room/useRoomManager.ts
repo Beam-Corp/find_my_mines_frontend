4e2f3be55e0dbe56ebb2904b39266561b1c60c2b
Sippakorn Ornwichian <o.sippakorn@gmail.com>
2021-10-21 21:45:26 +0700

    feat: sort player to assign host authority

--- utils/room/useRoomManager.ts
+++ utils/room/useRoomManager.ts
@@ -1,38 +1,52 @@
-import router from 'next/dist/client/router';
-import * as React from 'react';
-import { useState,useEffect,useCallback } from 'react';
-import { Socket } from 'socket.io-client';
-import { RoomEvents } from './room.event';
+import * as React from 'react'
+import { useState, useEffect, useCallback } from 'react'
 
-export const useRoomManager = (currentPlayer: string,roomId:string,socket: Socket) => {
-    const [players, setPlayers] = useState<string[]>([currentPlayer])
-    const addPlayer = useCallback(
-        (name: string) => {
-          setPlayers((prev) => prev.concat([name]))
-        },
-        [setPlayers]
-      )
-      const removePlayer = useCallback((name: string) => {
-        setPlayers((prev) => prev.filter((n) => n !== name))
-      },[])
-      const onDisconnect = useCallback(() => {
-        socket.emit(RoomEvents.ON_LEAVE, { roomId, username: currentPlayer })
-        router.push('/')
-      },[currentPlayer,roomId,socket])
-      useEffect(() => {
-        socket.on(RoomEvents.LEAVE, removePlayer)
-        return () => {
-          socket.off(RoomEvents.LEAVE, removePlayer)
-        }
-      }, [socket, players,removePlayer])
-      useEffect(() => {
-        if (players.length < 2) socket.on(RoomEvents.INTRODUCE, addPlayer)
-        return () => {
-          socket.off(RoomEvents.INTRODUCE, addPlayer)
-        }
-      }, [addPlayer, socket, players.length])
-      useEffect(() => {
-        socket.emit(RoomEvents.INTRODUCE, { roomId, username: currentPlayer })
-      }, [players, socket, roomId, currentPlayer])
-    return {players,onDisconnect}
+import router from 'next/dist/client/router'
+
+import { Socket } from 'socket.io-client'
+
+import { RoomEvents } from './room.event'
+
+export const useRoomManager = (
+  currentPlayer: string,
+  roomId: string,
+  socket: Socket
+) => {
+  const [players, setPlayers] = useState<string[]>([currentPlayer])
+  const addPlayer = useCallback(
+    (name: string) => {
+      setPlayers((prev) => {
+        return prev[0].substring(0, 1) === '1'
+          ? prev.concat([name])
+          : [name].concat(prev)
+      })
+    },
+    [setPlayers]
+  )
+  const removePlayer = useCallback((name: string) => {
+    setPlayers((prev) => prev.filter((n) => n !== name))
+  }, [])
+  const onDisconnect = useCallback(() => {
+    socket.emit(RoomEvents.ON_LEAVE, { roomId, username: currentPlayer })
+    router.push('/')
+  }, [currentPlayer, roomId, socket])
+
+  useEffect(() => {
+    socket.on(RoomEvents.LEAVE, removePlayer)
+    return () => {
+      socket.off(RoomEvents.LEAVE, removePlayer)
+    }
+  }, [socket, players, removePlayer])
+
+  useEffect(() => {
+    if (players.length < 2) socket.on(RoomEvents.INTRODUCE, addPlayer)
+    return () => {
+      socket.off(RoomEvents.INTRODUCE, addPlayer)
+    }
+  }, [addPlayer, socket, players.length])
+
+  useEffect(() => {
+    socket.emit(RoomEvents.INTRODUCE, { roomId, username: currentPlayer })
+  }, [players, socket, roomId, currentPlayer])
+  return { players, onDisconnect }
 }

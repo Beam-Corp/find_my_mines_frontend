e6bc5144da716402eacd3c7ba5f2ca15c5f98129
Jirat Viriyataranon <jiratsun.11@gmail.com>
2021-10-24 17:00:19 +0700

    add lines component

--- components/Game/TitlePage/index.tsx
+++ components/Game/TitlePage/index.tsx
@@ -1,8 +1,11 @@
 import Lines from './Lines'
+import { useThemeContext } from '../../../useContext/useThemeContext'
 
 const TitlePage = () => {
+    const { themeColor } = useThemeContext()
     return (
         <div>
+            <Lines centerColor={themeColor.primary} sideColor={themeColor.secondary}/>
         </div>
     )
 }
--- pages/title.tsx
+++ pages/title.tsx
@@ -1,9 +1,9 @@
 import { NextPage } from 'next'
+import TitlePage from '../components/Game/TitlePage'
 
 const Title: NextPage = () => {
     return (
-        <>
-        </>
+        <TitlePage />
     )
 }
 

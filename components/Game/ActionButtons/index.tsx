310d0f2e4c1ec9ccc60be22421f41fc535859216
awesomeman007 <suparuek.ping@gmail.com>
2021-10-27 22:48:06 +0700

    win-lose-surrender logic

--- components/Game/ActionButtons/index.tsx
+++ components/Game/ActionButtons/index.tsx
@@ -1,48 +1,47 @@
 import React, { FC } from 'react'
+
 import styled from 'styled-components'
-import { Button } from '../../Button'
+
 import { useThemeContext } from '../../../useContext/useThemeContext'
+import { Button } from '../../Button'
 
 const SurrenderButton = styled.button`
-        position: absolute;
-        right: 30px;
-        padding: 10px 15px;
-        color: gray;
-        background-color: darkgray;
-        border-radius: 15px;
-        font-size: 20px;
-        border: transparent;
-        &:hover {
-            transition: 0.5s;
-            color: white;
-        }
-        &:not(:hover) {
-            transition: 0.5s;
-            color: gray;
-          }
-    `
+  position: absolute;
+  right: 30px;
+  padding: 10px 15px;
+  color: gray;
+  background-color: darkgray;
+  border-radius: 15px;
+  font-size: 20px;
+  border: transparent;
+  &:hover {
+    transition: 0.5s;
+    color: white;
+  }
+  &:not(:hover) {
+    transition: 0.5s;
+    color: gray;
+  }
+`
 
 interface ActionButton {
-    setGameResult: (result: "WIN" | "LOSE" | "DRAW") => void
+  // setGameResult: (result: "WIN" | "LOSE" | "DRAW") => void
+  onSurrender: () => void
 }
 
-const ActionButtons:FC<ActionButton> = ({setGameResult}) => {
-    const surrenderModal = () => {
+const ActionButtons: FC<ActionButton> = ({ onSurrender }) => {
+  const surrenderModal = () => {}
 
-    }
-    
-    const handleClick = () => {
-        setGameResult("LOSE")
-    }
+  const handleClick = () => {
+    // setGameResult("LOSE")
+    onSurrender()
+  }
 
-    return (
-        <>
-
-        <SurrenderButton onClick={handleClick}>
-            Surrender
-        </SurrenderButton>
-        </>
-    )
+  return (
+    <>
+      <SurrenderButton onClick={handleClick}>Surrender</SurrenderButton>
+    </>
+  )
 }
 
 export default ActionButtons
--- utils/game/game.event.ts
+++ utils/game/game.event.ts
@@ -4,12 +4,14 @@ export enum GameEvents {
   SELECT_BLOCK = 'select-block',
   TIME_UP = 'time-up',
   END_GAME = 'end-game',
+  SURRENDER = 'surrender',
 
   // server to client
   ON_STARTED = 'on-started',
   ON_SELECTED = 'on-selected',
   ON_TIME_UP = 'on-time-up',
   ON_GAME_END = 'on-game-end',
+  ON_SURRENDER = 'on-surrender',
 }
 
 export interface GameState {
@@ -17,4 +19,10 @@ export interface GameState {
   gridState: number[][]
   scoreState: number[]
   playerTurn: number
+  clickNumber: number
+}
+
+export interface SurrenderState {
+  roomId: string
+  surrenderer: number
 }

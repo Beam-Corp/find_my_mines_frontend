2aa1256e3151a85d5a47c0b068d1e456a32b53f4
Sippakorn Ornwichian <o.sippakorn@gmail.com>
2021-10-28 11:28:03 +0700

    fix: incorrect enum name

--- utils/room/room.event.ts
+++ utils/room/room.event.ts
@@ -1,11 +1,11 @@
 export enum RoomEvents {
-  CREATE = "create",
-  ON_CREATE = "on-create",
-  LEAVE = "leave",
-  ON_LEAVE = "on-leave",
-  JOIN = "join",
+  CREATE = 'create',
+  ON_CREATE = 'on-create',
+  LEAVE = 'leave',
+  ON_LEAVE = 'on-leave',
+  JOIN = 'join',
   ON_JOIN = 'on-join',
-  INTRODUCE = "introduce",
-  GET_PLAYERS = "get-players",
-  ON_GET_PALYERS="on-get-players"
+  INTRODUCE = 'introduce',
+  GET_PLAYERS = 'get-players',
+  ON_GET_PLAYERS = 'on-get-players',
 }

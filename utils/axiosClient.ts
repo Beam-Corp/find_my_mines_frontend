d846105c57af33c29434b8fa0c4a3fc1c983257f
siradheek <siradhee_k@hotmail.com>
2021-10-28 15:32:33 +0700

    add axios config & usePlayerContext

--- utils/axiosClient.ts
+++ utils/axiosClient.ts
@@ -1,2 +1,2 @@
 import Axios from 'axios';
-export const client = Axios.create({baseURL: process.env.API_URL || "http://localhost:8000"})
+export const client = Axios.create({baseURL: process.env.API_URL || "http://localhost:8000",withCredentials: true})
--- utils/usePlayerContext.tsx
+++ utils/usePlayerContext.tsx
@@ -1,5 +1,9 @@
-import React from 'react'
+import React, { useCallback, useEffect } from 'react'
 import { createContext, useContext, useState } from 'react'
+
+import Axios from 'axios'
+
+import { client } from './axiosClient'
 
 interface Customization {
   color: string
@@ -9,7 +13,7 @@ interface PlayerInfo {
   userId: string
   role?: string
 }
-interface Player extends PlayerInfo {
+export interface Player extends PlayerInfo {
   customization: Customization
 }
 interface PlayerContextProps {
@@ -27,16 +31,30 @@ export const usePlayerContext = () => useContext(PlayerContext)
 
 export const PlayerProvider = ({ ...props }) => {
   const [player, setPlayer] = useState<Player>({} as Player)
-  const setPlayerInfo = (i: PlayerInfo) => {
+  useEffect(() => {
+    const source = Axios.CancelToken.source()
+    client
+      .get<Player>('/auth/verify', { cancelToken: source.token })
+      .then(({ data }) => {
+        setPlayer(data)
+      })
+      .catch((err) => {
+        if (Axios.isCancel(err)) console.log('cancelled')
+      })
+    return () => {
+      source.cancel()
+    }
+  }, [])
+  const setPlayerInfo = useCallback((i: PlayerInfo) => {
     setPlayer((prev) => ({
       ...prev,
       userId: i.userId,
       role: i.role || 'player',
     }))
-  }
-  const setPlayerCustom = (c: Customization) => {
+  }, [])
+  const setPlayerCustom = useCallback((c: Customization) => {
     setPlayer((prev) => ({ ...prev, customization: c }))
-  }
+  }, [])
   const value = {
     customization: player.customization,
     playerInfo: { role: player.role, userId: player.userId },

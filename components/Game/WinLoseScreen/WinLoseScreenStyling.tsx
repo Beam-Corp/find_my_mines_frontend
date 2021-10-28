44f0e4c078d3331ad476878134b4035c1855bd2b
Sippakorn Ornwichian <o.sippakorn@gmail.com>
2021-10-28 17:30:09 +0700

    fix: incorrect text alignment and font size

--- components/Game/WinLoseScreen/WinLoseScreenStyling.tsx
+++ components/Game/WinLoseScreen/WinLoseScreenStyling.tsx
@@ -29,7 +29,7 @@ const showBtn = keyframes`
   100% { opacity: 1; }
 `
 
-export const Overlay = styled.div<{ show: boolean, mounted: boolean }>`
+export const Overlay = styled.div<{ show: boolean; mounted: boolean }>`
   position: fixed;
   top: 0;
   left: 0;
@@ -42,14 +42,14 @@ export const Overlay = styled.div<{ show: boolean, mounted: boolean }>`
   transition: opacity 300ms linear;
 `
 
-export const Container = styled.div<{ show: boolean, mounted: boolean }>`
+export const Container = styled.div<{ show: boolean; mounted: boolean }>`
   display: ${(props) => (props.mounted ? 'initial' : 'none')};
   opacity: ${(props) => (props.show ? '1' : '0')};
   pointer-events: ${(props) => (props.show ? 'auto' : 'none')};
   transition: opacity 250ms linear;
 `
 
-export const Window = styled.div<{ show: boolean, theme: ThemeColorProps }>`
+export const Window = styled.div<{ show: boolean; theme: ThemeColorProps }>`
   position: fixed;
   top: 35%;
   left: 50%;
@@ -57,26 +57,34 @@ export const Window = styled.div<{ show: boolean, theme: ThemeColorProps }>`
   border-style: solid;
   border-width: 5px;
   border-radius: 30px;
-  border-color: ${({theme}) => theme.primary};
-  background-color: ${({theme}) => theme.background};
+  border-color: ${({ theme }) => theme.primary};
+  background-color: ${({ theme }) => theme.background};
   width: 45%;
   height: 45%;
   opacity: 0;
   display: flex;
   align-items: center;
   justify-content: center;
-  ${(props) => (props.show ? css`animation: ${enlarge} 250ms ease-out forwards;` : css`animation: ${minimize} 250ms ease-out forwards;`)}
+  ${(props) =>
+    props.show
+      ? css`
+          animation: ${enlarge} 250ms ease-out forwards;
+        `
+      : css`
+          animation: ${minimize} 250ms ease-out forwards;
+        `}
 `
 
-export const WinLose = styled.h1<{theme: ThemeColorProps}>`
-  color: ${({theme}) => theme.highlight};
+export const WinLose = styled.h1<{ theme: ThemeColorProps }>`
+  color: ${({ theme }) => theme.highlight};
   margin: 0;
-  font-size: 15vw;
-  padding-bottom: 3.5vh;
+  font-size: 40px;
+  padding: 16px;
+  text-align: center;
 
   ::before {
     content: '';
-    background-color: ${({theme}) => theme.secondary};
+    background-color: ${({ theme }) => theme.secondary};
     width: 35px;
     height: 35px;
     border-radius: 50%;
@@ -87,7 +95,7 @@ export const WinLose = styled.h1<{theme: ThemeColorProps}>`
 
   ::after {
     content: '';
-    background-color: ${({theme}) => theme.secondary};
+    background-color: ${({ theme }) => theme.secondary};
     width: 35px;
     height: 35px;
     border-radius: 50%;
@@ -97,9 +105,9 @@ export const WinLose = styled.h1<{theme: ThemeColorProps}>`
   }
 `
 
-export const Button = styled.button<{ top: string, theme: ThemeColorProps }>`
+export const Button = styled.button<{ top: string; theme: ThemeColorProps }>`
   position: fixed;
-  top: ${(props) => (props.top)};
+  top: ${(props) => props.top};
   left: 50%;
   transform: translateX(-50%) translateY(-50%);
   width: 20%;
@@ -107,19 +115,14 @@ export const Button = styled.button<{ top: string, theme: ThemeColorProps }>`
   border-style: solid;
   border-width: 5px;
   border-radius: 30px;
-  border-color: ${({theme}) => theme.primary};
-  background-color: ${({theme}) => theme.background};
-  color: ${({theme}) => theme.secondary};
+  border-color: ${({ theme }) => theme.primary};
+  background-color: ${({ theme }) => theme.background};
+  color: ${({ theme }) => theme.secondary};
   font-size: 5vh;
   animation: ${showBtn} 250ms linear forwards;
 
   :hover {
-    border-color: ${({theme}) => theme.highlight};
+    border-color: ${({ theme }) => theme.highlight};
     transform: translateX(-50%) translateY(-50%) scale(1.1);
   }
 `
-
- 
-
-
-

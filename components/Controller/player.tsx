b36e2de5e644252283923914fdd021bf2b463b01
Sippakorn Ornwichian <o.sippakorn@gmail.com>
2021-11-02 15:07:45 +0700

    refactor: adjust audioPlayer to typescript format

--- components/Controller/player.tsx
+++ components/Controller/player.tsx
@@ -1,89 +1,91 @@
-import React, { useState, useEffect, useRef } from "react";
-import BGSong from "../../public/audioClips/Waterworld - Map.mp3";
-import MusicNote from "../../public/music.png";
-import NoMusicNote from "../../public/nomusic.png";
-import Image from 'next/image';
-import VercelIcon from "../../public/vercel.svg";
-import styles from '../../styles/Home.module.css';
+import React, { useState, useEffect, useRef, useCallback } from 'react'
 
-const useAudio = url => {
-  const [audio] = useState(new Audio(BGSong));
-  const [playing, setPlaying] = useState(false);
-  const [volume] = useState(audio.volume);
-  const toggle = () => {
-    setPlaying(!playing);
-    return false;
-  }
+import Image from 'next/image'
 
-  const changeVolume = (vol) => {
-    audio.volume = vol;
+import BGSong from '../../public/audioClips/Waterworld - Map.mp3'
+import MusicNote from '../../public/music.png'
+import NoMusicNote from '../../public/nomusic.png'
+import VercelIcon from '../../public/vercel.svg'
+import styles from '../../styles/Home.module.css'
+
+const useAudio = () => {
+  const [audio, setAudio] = useState(new Audio(BGSong))
+  const [playing, setPlaying] = useState<boolean>(false)
+  const [volume, setVolume] = useState(audio.volume)
+  const toggle = useCallback(() => {
+    setPlaying((prev) => !prev)
+  }, [])
+
+  const changeVolume = (vol: number) => {
+    setVolume(vol)
   }
 
   useEffect(() => {
-      if(playing) {
-        audio.play();
-      } else audio.pause();
-    },
-    [playing,audio]
-  );
+    audio.volume = volume
+  }, [audio, volume])
+
+  useEffect(() => {
+    if (playing) {
+      audio.play()
+    } else audio.pause()
+  }, [playing, audio])
 
   useEffect(() => {
     audio.addEventListener('ended', () => {
-      audio.currentTime = 0;
-      audio.play();
-      console.log('looping');
-    });
+      audio.currentTime = 0
+      audio.play()
+      console.log('looping')
+    })
     return () => {
       audio.removeEventListener('ended', () => {
-        audio.currentTime = 0;
-        audio.play();
-        console.log('looping');
-      });
-    };
-  }, [audio]);
+        audio.currentTime = 0
+        audio.play()
+        console.log('looping')
+      })
+    }
+  }, [audio])
 
-  return [playing, toggle, changeVolume];
-
-};
-
-var MusicIcon = MusicNote;
-
-const getImage = playing => {
-  playing ? MusicIcon = MusicNote : MusicIcon = NoMusicNote;
-  return MusicIcon;
+  return { playing, toggle, changeVolume }
 }
 
+var MusicIcon = MusicNote
 
-const Player = ({ url }) => {
-  const [playing, toggle, changeVolume] = useAudio(url);
+const getImage = (playing: boolean) => {
+  playing ? (MusicIcon = MusicNote) : (MusicIcon = NoMusicNote)
+  return MusicIcon
+}
 
-  const[volume, setVolume] = useState(1);
+const Player = () => {
+  const { playing, toggle, changeVolume } = useAudio()
 
-  const finalVolume = volume**2;
-
+  const [volume, setVolume] = useState(1)
 
   return (
-    <div className = {styles.slidecontainer}>
+    <div className={styles.slidecontainer}>
       {/* Music: <button onClick={toggle}>{playing ? "On" : "Off"}</button> */}
-      <span className = {styles.img}>
-      <Image onClick={toggle} src= {getImage(playing)} width={34} height={34} alt="Music"/>
+      <span className={styles.img}>
+        <Image
+          onClick={toggle}
+          src={getImage(playing)}
+          width={34}
+          height={34}
+          alt="Music"
+        />
       </span>
-      <input className = {styles.slider}
-          type="range"
-          min={0}
-          max={1}
-          step={0.02}
-          value={volume}
-          onChange={event => {
-            setVolume(event.target.valueAsNumber)
-            changeVolume(finalVolume.toFixed(3));
-          }}
+      <input
+        className={styles.slider}
+        type="range"
+        min={0}
+        max={1}
+        step={0.02}
+        value={volume}
+        onChange={(event) => {
+          setVolume(parseFloat(event.target.value))
+          changeVolume(parseFloat((volume ** 2).toFixed(3)))
+        }}
       />
-
     </div>
-    
-  );
-};
+  )
+}
 
-export default Player;
-
+export default Player

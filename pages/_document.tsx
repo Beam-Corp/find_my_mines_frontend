24e52335d7830ff2708b9a67bd793c797ad8808c
Sippakorn Ornwichian <o.sippakorn@gmail.com>
2021-11-02 10:48:24 +0700

    init: add _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,39 @@
+import { PropsWithChildren } from 'react'
+
+import { AppPropsType } from 'next/dist/shared/lib/utils'
+import Document, { DocumentContext, DocumentInitialProps } from 'next/document'
+import { NextRouter } from 'next/router'
+
+import { ServerStyleSheet } from 'styled-components'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp:
+            // eslint-disable-next-line react/display-name
+            (App) => (props: PropsWithChildren<AppPropsType<NextRouter, {}>>) =>
+              sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+}

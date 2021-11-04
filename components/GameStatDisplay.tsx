ec5941948beecf8b36cab7356bb995f9e59a90ce
Sippakorn Ornwichian <o.sippakorn@gmail.com>
2021-11-04 23:25:41 +0700

    feat: add game stat display

---
+++ components/GameStatDisplay.tsx
@@ -0,0 +1,91 @@
+import React, { FC, useCallback, useEffect, useState } from 'react'
+
+import styled from 'styled-components'
+
+import { ThemeColorProps } from '../dto/themeColor.dto'
+import { useThemeContext } from '../useContext/useThemeContext'
+import { client } from '../utils/axiosClient'
+import { usePlayerContext } from '../utils/usePlayerContext'
+import { Row, TextContainer } from './Container'
+import IconToggleButton from './IconToggleButton'
+
+interface ControllerProps {}
+
+const ControllerContainer = styled(Row)<{
+  hide: boolean
+  themeColor: ThemeColorProps
+}>`
+  position: absolute;
+
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  bottom: 160px;
+  left: ${({ hide }) => (!hide ? '0px' : '-248px')};
+  padding: 16px 8px 16px 16px;
+
+  width: 320px;
+
+  background: ${({ themeColor }) => themeColor.background};
+  border: ${({ themeColor }) => themeColor.secondary} 4px solid;
+  border-left: 0px;
+  border-radius: 0px 16px 16px 0px;
+  backdrop-filter: blur(5px);
+
+  color: ${({ themeColor }) => themeColor.secondary};
+
+  transition: left 500ms;
+`
+
+interface Statistics {
+  winCount: number
+  drawCount: number
+  loseCount: number
+}
+
+const GameStatDisplay: FC<ControllerProps> = () => {
+  const { themeColor } = useThemeContext()
+  const { playerInfo } = usePlayerContext()
+
+  const [hide, setHide] = useState<boolean>(false)
+
+  const [stat, setStat] = useState<Statistics>({
+    winCount: 0,
+    drawCount: 0,
+    loseCount: 0,
+  })
+
+  const getStats = useCallback(async () => {
+    const playerData = await client.get(
+      `/player/stats?userId=${playerInfo.userId.substring(2)}`
+    )
+    console.log(playerData.data)
+    setStat(playerData.data.statistics)
+  }, [playerInfo])
+
+  useEffect(() => {
+    if (playerInfo.userId) {
+      getStats()
+    }
+  }, [getStats])
+
+  return (
+    <>
+      <ControllerContainer themeColor={themeColor} hide={hide}>
+        <TextContainer size={4}>
+          {playerInfo?.userId?.substring(2)}
+        </TextContainer>
+        <TextContainer size={4}>
+          W/D/L: {stat.winCount}/{stat.drawCount}/{stat.loseCount}
+        </TextContainer>
+        <IconToggleButton
+          imagePaths={['/arrowLeft.svg', '/arrowRight.svg']}
+          handleToggle={() => setHide(!hide)}
+        />
+      </ControllerContainer>
+    </>
+  )
+}
+
+export default GameStatDisplay
